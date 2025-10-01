100d95376456bb5292d104564ff3d8412580ab8b
Robosiddhi <githubrobosiddhi@gmail.com>
2025-10-01 10:54:16 +0530

    Add files via upload

---
+++ linefollowerfinalcode.ino
@@ -0,0 +1,133 @@
+/*
+Code Name: Arduino line follower robot
+Author: RoboSiddhi
+
+⚠ Warning
+
+This project and its code are shared for learning and personal use only.  
+Commercial use, redistribution, or selling (in any form) of this code or
+project is not allowed without proper permission.  
+
+                                                                                 .                 
+              !^           .   ..   ..            
+              ??^~^.      ....:....             
+             :P5J?7?JY5.   ...^:::^..             
+            .BG5JY75&JYG   ...^:::^..             
+            .BBBP57?YYYG.   ....::::              
+             :YBBGYJ??Y?   .  .^ YGGY.            
+              :!BBBBY!:       .G??5BP^            
+            ~GP5P5??77!7:    .^J55Y!              
+           ^#57!7#5777!?57~:77JG                
+           ?#G5PB#57777!B5GG5YPJ.                 
+          .BGYY&&BPY???J!!Y5Y7:                   
+          :7!~!7777!!!!^     
+                               
+Description:  Line Follower Robot A simple Arduino Uno based line follower robot using 2 
+IR sensors, L298N motor driver, 4 BO motors with wheels, and a 2-cell 18650 battery pack.
+The sensors detect the path and Arduino controls the motors to follow the line. Great for
+learning robotics, DIY projects, and basic STEM applications.
+*/
+#define in1 9
+#define in2 8
+#define in3 7
+#define in4 6
+#define enA 10
+#define enB 5
+
+
+ int M1_Speed = 80; // speed of motor 1
+ int M2_Speed = 80; // speed of motor 2
+ int LeftRotationSpeed = 250;  // Left Rotation Speed
+ int RightRotationSpeed = 250; // Right Rotation Speed
+
+
+ void setup() {
+
+  pinMode(in1,OUTPUT);
+  pinMode(in2,OUTPUT);
+  pinMode(in3,OUTPUT);
+  pinMode(in4,OUTPUT);
+
+    pinMode(enA,OUTPUT);
+    pinMode(enB,OUTPUT);
+
+      pinMode(A0, INPUT); // initialize Left sensor as an input
+      pinMode(A1, INPUT); // initialize Right sensor as an input
+
+}
+
+void loop() {
+
+  int LEFT_SENSOR = digitalRead(A0);
+  int RIGHT_SENSOR = digitalRead(A1);
+
+if(RIGHT_SENSOR==0 && LEFT_SENSOR==0) {
+    forward(); //FORWARD
+}
+
+  else if(RIGHT_SENSOR==0 && LEFT_SENSOR==1) {
+    right(); //Move Right
+ }
+
+  else if(RIGHT_SENSOR==1 && LEFT_SENSOR==0) {
+    left(); //Move Left
+}
+
+  else if(RIGHT_SENSOR==1 && LEFT_SENSOR==1) {
+    Stop();  //STOP
+ }
+}
+
+
+
+void forward()
+{
+            digitalWrite(in1, HIGH);
+            digitalWrite(in2, LOW);
+            digitalWrite(in3, HIGH);
+            digitalWrite(in4, LOW);
+
+                analogWrite(enA, M1_Speed);
+                analogWrite(enB, M2_Speed);
+}
+
+void backward()
+{
+            digitalWrite(in1, LOW);
+            digitalWrite(in2, HIGH);
+            digitalWrite(in3, LOW);
+            digitalWrite(in4, HIGH);
+
+                analogWrite(enA, M1_Speed);
+                analogWrite(enB, M2_Speed);
+}
+
+void right()
+{
+            digitalWrite(in1, LOW);
+            digitalWrite(in2, HIGH);
+            digitalWrite(in3, HIGH);
+            digitalWrite(in4, LOW);
+
+                analogWrite(enA, LeftRotationSpeed);
+                analogWrite(enB, RightRotationSpeed);
+}
+
+void left()
+{
+            digitalWrite(in1, HIGH);
+            digitalWrite(in2, LOW);
+            digitalWrite(in3, LOW);
+            digitalWrite(in4, HIGH);
+
+                analogWrite(enA, LeftRotationSpeed);
+                analogWrite(enB, RightRotationSpeed);
+}
+
+void Stop()
+{
+            digitalWrite(in1, LOW);
+            digitalWrite(in2, LOW);
+            digitalWrite(in3, LOW);
+            digitalWrite(in4, LOW);
+}
